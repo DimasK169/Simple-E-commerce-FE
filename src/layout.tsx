185cdf2b71e569ee82c6f0b36b84fb3d93aa7744
Dimas Kenang Surya Ardani <dimas.jdt15@gmail.com>
2025-04-16 11:22:45 +0700

    add header

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Header from "./component/header/header";
 
 const Layout = () => {
   return (
-    <div className='underline'>Test Tailwind</div>
-  )
-}
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="grow"></div>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
