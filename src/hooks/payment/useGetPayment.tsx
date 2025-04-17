cb9ded7785caed7ce573fde6beade66875a80edd
Anugrah <anugrahnunu858@gmail.com>
2025-04-17 14:09:40 +0700

    First Commit: FE Payment

---
+++ src/hooks/payment/useGetPayment.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { getPayment } from "../../services/payment/finishedPayment/api";
+
+export const useGetPayment = () => {
+  const [payment, setPayment] = useState();
+
+  const fetchPayment = async () => {
+    try {
+      const response = await getPayment();
+      setPayment(response?.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPayment();
+  }, []);
+
+  return { payment };
+};
